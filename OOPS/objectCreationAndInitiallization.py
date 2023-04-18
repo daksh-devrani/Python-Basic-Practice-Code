45aacf135094a8b1ae90f2926c0adb266f40d992
daksh-devrani <129187923+daksh-devrani@users.noreply.github.com>
2023-04-18 13:35:13 +0530

    Delete OOPS directory

--- OOPS/objectCreationAndInitiallization.py
+++
@@ -1,8 +0,0 @@
-class Student:
-    def __init__(self,name,school):
-        self.name=name
-        self.school=school
-
-daksh=Student('Daksh','Alma Mater')
-print(daksh.name)
-print(daksh.school)
